6aba94eb09348308409d3750c665b0ec6816105d
Willummp <lsousaah@gmail.com>
2022-09-08 16:57:23 -0300

    Exercicios

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Variables-javaScript
 Exercicio de Variaveis JavaScript
+
+nome: STRING
+
+sobrenome: STRING
+
+peso : NUMBER 
+
+altura: NUMBER 
+
+dataDeInternacao: STRING
+
+diabético: BOOLEAN
+
---
+++ patientInfo.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+
+const weight = prompt('Type your weight ')
+
+
+const name = prompt('Type your name ')
+
+
+const height = prompt('Type your height')
+
+
+const hospitali = prompt('On what date were you hospitalized?')
+
+
+const diabetes = prompt('If you have DIABETES type Y/N')
